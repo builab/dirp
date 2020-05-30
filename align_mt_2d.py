e447affe9a117717e19fb0773fbd8f1d3499aa1a
builab <huy.bui@mcgill.ca>
2020-05-30 01:53:41 -0400

    Update align_mt_2d.py

--- align_mt_2d.py
+++ align_mt_2d.py
@@ -20,7 +20,7 @@ def align2d(starfile, outprefix):
 	"""Performing align 2d of mt star file using relion"""
 	align2d = "relion_refine --i " + starfile + " --o " + outprefix + " --dont_combine_weights_via_disc --no_parallel_disc_io --preread_images --pool 200 --pad 2 --ctf --iter 6 --tau2_fudge 12 --particle_diameter 600 --K 1 --flatten_solvent --oversampling 1 --psi_step 2 --offset_range 10 --offset_step 2 --helical_outer_diameter 500 --sigma_psi 5 --dont_check_norm --norm --scale --j 4"
 	print(align2d)
-	#os.system(align2d)
+	os.system(align2d)
 	
 
 if __name__=='__main__':
