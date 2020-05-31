32aa516c563bce941c7d3bc1f3f430cd97fa5023
builab <huy.bui@mcgill.ca>
2020-05-31 01:57:18 -0400

    Update xform_avg_2d.py

--- xform_avg_2d.py
+++ xform_avg_2d.py
@@ -87,8 +87,11 @@ if __name__=='__main__':
 		applytransformation(starfile, outdir + "/" + basename)
 		averagestack(outdir + "/" + basename + ".mrcs", outdir + "/" + basename + "_avg.mrcs")
 		
+	
 	# Create average star file
+	print ("Create output " + args.ostar + "from " + outdir + "/*.star")
 	listxformstar=glob.glob(outdir + "/*.star")
+	sys.exit(0)
    	starfile = listxformstar[0]
     	instar = open ( starfile, 'r')
     	starlabels = learnstarheader(instar)
