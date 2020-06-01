6faa9d1ce7a26da1d875b34cd4e89501e6bde6bb
builab <huy.bui@mcgill.ca>
2020-05-31 21:40:25 -0400

    Update slurm_submit_align_mt_2d.sh

--- slurm_submit_align_mt_2d.sh
+++ slurm_submit_align_mt_2d.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --ntasks=10                   # Number of processes
 #SBATCH --partition=cpu
-#SBATCH --job-name=xform_avg # Job name
-#SBATCH --error=xform_avg.err
-#SBATCH --output=xform_avg.out
+#SBATCH --job-name=align_2d # Job name
+#SBATCH --error=align_2d.err
+#SBATCH --output=align_2d.out
 #SBATCH --nodes=1                    # Run all processes on a single node	
-#SBATCH --mem-per-cpu=1GB
+#SBATCH --mem-per-cpu=5GB
 
 module load python-2.7.14-gcc-4.9.4-tzsdd67
 module load relion/3.0b-jan19
