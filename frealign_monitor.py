01a9d443db32caf5a1d505940b0c8a39c1831252
builab <huy.bui@mcgill.ca>
2020-06-19 14:16:44 -0400

    Add files via upload

---
+++ frealign_monitor.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 13:19:02 2020
+Script sequentially run through a list of mparameters file in Frealign
+@author: kbui2
+"""
+
+import shutil, time, os.path, sys
+import subprocess
+
+
+def launchfrealign():
+	''' Launch free align '''
+	process = subprocess.Popen(['frealign_run_refine', ''])
+	return process
+
+def checkprocessrunning(pid):
+	if os.path.exists('/proc/' + str(pid)):
+		return 1
+	else:
+		return 0
+	
+if __name__ == "__main__":
+	# Checking interval
+	interval = 300
+	
+	if len(sys.argv) < 2:
+		print "Usage: python ./frealign_monitor.py mparameter1 mparameter2 mparametere3"
+	
+	mparamlist = sys.argv[1:]
+
+	for i in range(len(mparamlist)):
+		shutil.copy(mparamlist[i], 'mparameters')
+		process = launchfrealign()
+		while True:
+			if checkprocessrunning(process.pid) == 1:
+				print('Process ' + str(process.pid) + ' still alive ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+			else:
+				print('Process ' + str(process.pid) + ' done')
+				break
+			time.sleep(interval)
+			
+		
+		
+		
