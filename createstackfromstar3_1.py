5184c3de02fa48dd57774b446d94efc3b2735268
builab <huy.bui@mcgill.ca>
2021-11-04 21:58:29 -0400

    Update createstackfromstar3_1.py

--- createstackfromstar3_1.py
+++ createstackfromstar3_1.py
@@ -81,10 +81,13 @@ def learnstarheader(infile):
     infile.seek(0) # Go to the beginning of the starfile
     doneheader = False
     doneprelabels = False
-    doneoptics = False
+    doneoptics = True
     headerlabels = []
     while not doneprelabels:
         line=infile.readline()
+        # Check if star 3.1 format
+        if line.startswith('data_optics'):
+            doneoptics = False
         if line.startswith('data_particles'):
             doneoptics = True
         if line.startswith('loop_') & doneoptics == True:
