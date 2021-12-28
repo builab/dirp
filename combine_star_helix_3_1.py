b4a812056da6308b5f8e11ef85992207d6c93bd4
builab <huy.bui@mcgill.ca>
2021-12-27 21:20:17 -0500

    Update combine_star_helix_3_1.py

--- combine_star_helix_3_1.py
+++ combine_star_helix_3_1.py
@@ -13,32 +13,52 @@ import argparse
 
 from eulerangles import euler2matrix
 
-def renumber_helicalID(df):
-
+def renumber_helicalID(df_part):
+	count=1
+	helicalid = 0
+	prevhelicalid = 0
+	prevmicroname = ''
+	df_renumber = df_part.copy()
+	for i in range(len(df_part)):
+		microname=df_part.loc[i, 'rlnMicrographName']
+		micronum = os.path.basename(microname)
+		# This is highly dependent on the micrograph name, need to fix it in the future
+		# Do a temporary fix for now. Asumming file name is xxxx_01234_1-3.mrc
+		#micronum = re.sub('.*_(\d\d\d\d\d).mrc', '\\1', micronum)
+		#print(micronum)
+		#micronum = int(micronum)
+		#print(record[psipriorcol])
+		if microname != prevmicroname:
+			prevmicroname = microname
+			helicalid += 1
+			prevhelicalid = df_part.loc[i, 'rlnHelicalTubeID']	
+		if prevhelicalid != df_part.loc[i, 'rlnHelicalTubeID']:
+			helicalid += 1
+			prevhelicalid = df_part.loc[i, 'rlnHelicalTubeID']
+		df_renumber.loc[i, 'rlnHelicalTubeID'] = helicalid
+		
+	return df_renumber
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser(description='Combine helical star files. Assuming same optic groups')
 	parser.add_argument('--i1', help='Input star file 1',required=True)
-	parser.add_argument('--i2', help='Input star file 2',required=True)
+	#parser.add_argument('--i2', help='Input star file 2',required=True)
 	parser.add_argument('--o', help='Output star file',required=True)
 
 	
 	args = parser.parse_args()
-	
-	outfile = args.o
-	
+		
 
 	stardict1 = starfile.read(args.i1)
 	
 	df_optics1 = stardict1['optics']	
 	
 	df_particles1 = stardict1['particles']
-
+	
+	df_renumber1 = renumber_helicalID(df_particiles1)
+	
+	stardict1['particles'] = df_renumber1
 	
 	# Offset to load in case many different object. Not use now
-	out = open(outfile, 'w')
+	starfile.write(stardict1, args.o)
 	
-
-
-	
-	out.close()
