1e1adf4bf9c989d56caa1ba302ebf85ee7a74775
builab <huy.bui@mcgill.ca>
2021-12-27 22:23:34 -0500

    Update combine_star_helix_3_1.py

--- combine_star_helix_3_1.py
+++ combine_star_helix_3_1.py
@@ -11,9 +11,9 @@ import numpy as np
 import starfile
 import argparse
 
-def renumber_helicalID(df_part):
+def renumber_helicalID(df_part, offset):
 	count=1
-	helicalid = 0
+	helicalid = 0 + offset
 	prevhelicalid = 0
 	prevmicroname = ''
 	df_renumber = df_part.copy()
@@ -46,16 +46,19 @@ if __name__=='__main__':
 	args = parser.parse_args()
 		
 	stardict1 = starfile.read(args.i1)
-	
 	df_optics1 = stardict1['optics']	
-	
 	df_particles1 = stardict1['particles']
+	df_renumber1 = renumber_helicalID(df_particles1, 0)
 	
-	print(df_particles1)
+	stardict2 = starfile.read(args.i2)
+	df_optics2 = stardict2['optics']	
+	df_particles2 = stardict2['particles']
+	offset = df_renumber1.loc[len(df_part) - 1, 'rlnHelicalTubeID']
+	df_renumber2 = renumber_helicalID(df_particles2, offset)
+
+	df_new = df_renumber1.append(df_renumber2, ignore_index=True)
+	stardict1['particles'] = df_new
 	
-	df_renumber1 = renumber_helicalID(df_particles1)
-	
-	stardict1['particles'] = df_renumber1
 	
 	# Offset to load in case many different object. Not use now
 	starfile.write(stardict1, args.o)
